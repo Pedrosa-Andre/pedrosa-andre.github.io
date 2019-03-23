5af39941af106cb226de188f071f073bd4ab3e7d
pedrosa-andre <andre.matheus.pedrosa@gmail.com>
2019-03-23 00:25:53 -0300

    Folder 11 started

---
+++ assignments/weathersite/scripts/currentdate.js
@@ -0,0 +1,11 @@
+var dt = new Date();
+var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday",
+"Saturday"]
+var wd = weekday[dt.getDay()];
+var da = dt.getDate();
+var month = ["January","February","March","April","May","June","July"
+,"August","September","October","November","December"];
+var mo = month[dt.getMonth()];
+var ye = dt.getFullYear();
+var x = "fffff";
+document.getElementById("foo").innerHTML = wd + ", " + da + " " + mo + " " + ye;
---
+++ assignments/weathersite/scripts/weather5foracast.js
@@ -0,0 +1,28 @@
+
+//var requestURL2 = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=2860384eba6f003db94da4484cbcf9fd&units=imperial";
+var requestURL2 = "https://pedrosa-andre.github.io/assignments/lesson-10/scripts/preston-w5.json";
+var request2 = new XMLHttpRequest();
+request2.open('GET', requestURL2, true);
+request2.responseType = 'json';
+request2.send();
+
+var weekdayShort = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat","Sun","Mon","Tue","Wed","Thu"]
+
+request2.onload = function() {
+    var fiveDayInfo = request2.response;
+    console.log(fiveDayInfo);
+    fiveDayForecast(fiveDayInfo);
+}
+function fiveDayForecast (jsonObj) {
+    var weatherInfo = jsonObj['list'];
+    document.getElementById("tem1").textContent= weatherInfo[0].main.temp;
+    document.getElementById("tem2").textContent= weatherInfo[7].main.temp;
+    document.getElementById("tem3").textContent= weatherInfo[15].main.temp;
+    document.getElementById("tem4").textContent= weatherInfo[23].main.temp;
+    document.getElementById("tem5").textContent= weatherInfo[31].main.temp;
+    document.getElementById("day1").textContent= weekdayShort[dt.getDay()+1];
+    document.getElementById("day2").textContent= weekdayShort[dt.getDay()+2];
+    document.getElementById("day3").textContent= weekdayShort[dt.getDay()+3];
+    document.getElementById("day4").textContent= weekdayShort[dt.getDay()+4];
+    document.getElementById("day5").textContent= weekdayShort[dt.getDay()+5];
+}
