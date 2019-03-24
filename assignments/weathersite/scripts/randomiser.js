5287e68b656e5e41c2c8a2595fb49a5d755fbe4b
Pedrosa-Andre <43426242+Pedrosa-Andre@users.noreply.github.com>
2019-03-24 01:27:59 -0300

    Delete randomiser.js

--- assignments/weathersite/scripts/randomiser.js
+++
@@ -1,7 +0,0 @@
-temp = parseFloat((Math.random() * 50)-20).toFixed(1);
-temperature.setAttribute('vall',temp);
-document.getElementById('temperature').innerHTML = temp + "&#176F";
-
-spee = parseFloat((Math.random() * 30)).toFixed(1);
-speed.setAttribute('vall',spee);
-document.getElementById('speed').innerHTML = spee + " mi/h";
